f5ceb96fd2224dd581ab56bcdcbbc28b3c01c0c6
Jugutier <julian.3.1@gmail.com>
2014-05-06 10:32:56 -0300

    Finished

--- TPE1/src/TPE1.m
+++ TPE1/src/TPE1.m
@@ -1,3 +1,28 @@
 function solution = TPE1()
-
+	load initializeGlobals.m;
+	load train.m;
+	load transmit.m;
+	initializeGlobals();
+	global h;
+	global L;
+	filename = '../img/lena512.bmp';
+	s = double(imread(filename)(1,L));
+	M = columns(s);
+	S = toeplitz([zeros(1,M-L) s'],zeros(1,M));
+	r = train(S);
+	h = r/S;%desp cholesy y subst hacia atras
+	a = double(imread(filename));
+	L= rows(h);
+	M = columns(a);
+	Sfinal = [];
+	for i=1:columns(a)
+		r = transmit(a(:,i));
+		H = toeplitz([zeros(1,M-L) h'],zeros(1,M));
+		s= r/H;
+		Sfinal = [Sfinal s];
+	endfor
+	Sfinal = uint8(Sfinal);
+	imshow(Sfinal);
+	
+	
 endfunction
