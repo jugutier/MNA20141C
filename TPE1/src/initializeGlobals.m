638964248b3131f3d76211eb1f0e3e16abb25ad6
Jugutier <julian.3.1@gmail.com>
2014-05-06 14:16:44 -0300

    Changes

--- TPE1/src/initializeGlobals.m
+++ TPE1/src/initializeGlobals.m
@@ -3,5 +3,5 @@ function  initializeGlobals()
 	global L;
 	L = 3;
 	ganancia = 1/10;
-	h = ganancia * (1+randn(L,1));
+	h = ganancia * (1+randn(L,1));%%this h is unknown, we want to estimate it.
 endfunction
